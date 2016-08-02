3e817e9dce2f1328c053909c02358710655c3db2
marufr <maruf.r@outlook.com>
2016-08-02 16:18:31 +1000

    updated to match usage of modified 'systemlayout.py'

--- sifra/sifraclasses.py
+++ sifra/sifraclasses.py
@@ -199,13 +199,23 @@ class Network(object):
                        weight=row['weight'])
 
         if facility.system_class.lower() in ['potablewatertreatmentplant']:
-            systemlayout.draw_wtp_layout(
-                sys, comp_df, out_dir=facility.output_path,
-                graph_label="Water Treatment Plant Component Layout")
+            systemlayout.draw_sys_layout(
+                sys, comp_df,
+                out_dir=facility.output_path,
+                graph_label="Water Treatment Plant Component Layout",
+                orientation = "TB",
+                connector_type="ortho",
+                clustering=True
+            )
         else:
             systemlayout.draw_sys_layout(
-                sys, comp_df, out_dir=facility.output_path,
-                graph_label="System Component Layout")
+                sys, comp_df,
+                out_dir=facility.output_path,
+                graph_label="System Component Layout",
+                orientation="LR",
+                connector_type="spline",
+                clustering=False
+            )
 
         # ---------------------------------------------------------------------
         # List of tagged nodes with special roles:
