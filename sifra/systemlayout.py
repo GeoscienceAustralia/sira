da629b41a5aeb8f143923f87149907d9b7706d5d
marufr <maruf.r@outlook.com>
2016-08-02 16:16:42 +1000

    draw_sys_layout updated to allow customisation for different infrastructure types

--- sifra/systemlayout.py
+++ sifra/systemlayout.py
@@ -4,7 +4,7 @@ import sys
 import networkx as nx
 import pandas as pd
 
-# ------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 def msplit(string, delims):
     s = string
@@ -16,7 +16,8 @@ def msplit(string, delims):
 
 def segment_long_labels(string, chars=7, delims=[]):
     if (not delims) or \
-            (len(string) > chars and len(string.split(delims[0])[0]) > 8):
+            (len(string) > chars
+             and len(string.split(delims[0])[0]) > chars+1):
         return '\n'.join(
             string[i:i + chars] for i in range(0, len(string), chars))
     elif len(string) > chars:
@@ -25,100 +26,16 @@ def segment_long_labels(string, chars=7, delims=[]):
         return string
 
 
-# ------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
-def draw_sys_layout(G, comp_df, out_dir="", out_file="system_layout",
-                    graph_label="System Layout"):
-    """Draws the component configuration for a general infrastructure system."""
-
-    # --- Set up output file names & location ---
-    if not out_dir.strip():
-        output_path = os.getcwd()
-    else:
-        output_path = out_dir
-
-    fn = out_file.split(os.extsep)[0]  # strip away file ext and add our own
-    sys_config_dot = os.path.join(output_path, fn + '.dot')
-    sys_config_img = os.path.join(output_path, fn + '.png')
-
-    # --- Draw graph using pygraphviz ---
-
-    A = nx.nx_agraph.to_agraph(G)
-
-    A.graph_attr.update(resolution=300,
-                        size="10.25,7.75",
-                        directed=True,
-                        labelloc="t",
-                        label=graph_label,
-                        rankdir="LR",
-                        splines="spline",
-                        center="true",
-                        forcelabels="true",
-                        fontname="Helvetica-Bold",
-                        fontcolor="#444444",
-                        fontsize=18)
-
-    A.node_attr.update(shape="circle",
-                       style="rounded,filled",
-                       fixedsize="true",
-                       width=1.4,
-                       height=1.4,
-                       xlp="0, 0",
-                       color="royalblue2",
-                       fillcolor="white",
-                       fontcolor="royalblue3",
-                       penwidth=1.0,
-                       fontname="Helvetica-Bold",
-                       fontsize=14)
-
-    A.edge_attr.update(arrowhead="normal",
-                       arrowsize="1.0",
-                       color="royalblue2",
-                       penwidth=1.0)
-
-    # --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-
-    for node in comp_df.index.values.tolist():
-
-        label_mod = segment_long_labels(node, delims=['_', ' '])
-        A.get_node(node).attr['label'] = label_mod
-
-        if str(comp_df.ix[node]['node_type']) == 'supply':
-            A.get_node(node).attr.update(
-                label="",
-                xlabel=label_mod,
-                shape='point',
-                height=0.4,
-                style='filled',
-                fillcolor="royalblue2")
-
-        if str(comp_df.ix[node]['node_type']) == 'sink':
-            A.get_node(node).attr['shape'] = 'doublecircle'
-            A.get_node(node).attr['rank'] = 'sink'
-            A.get_node(node).attr['penwidth'] = 2.0
-
-        if str(comp_df.ix[node]['node_type']) == 'dependency':
-            A.get_node(node).attr.update(
-                shape="circle",
-                penwidth=2.0,
-                color="Orchid",
-                fontcolor="Orchid")
-
-    for cluster in pd.unique(comp_df['node_cluster'].values):
-        grp = comp_df[comp_df['node_cluster'] == cluster].index.values.tolist()
-        A.add_subgraph(grp, rank='same')
-
-    # --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-
-    A.write(sys_config_dot)
-    A.draw(sys_config_img, format='png', prog='dot')
-
-
-# ******************************************************************************
-
-def draw_wtp_layout(G, comp_df, out_dir="", out_file="system_layout",
-                    graph_label="System Layout"):
-    """Draws the component configuration for water treatment plants."""
+def draw_sys_layout(G, comp_df,
+                    out_dir="",
+                    out_file="system_layout",
+                    graph_label="System Layout",
+                    orientation="TB",
+                    connector_type="spline",
+                    clustering=False):
+    """Draws the component configuration for given infrastructure system."""
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     # Set up output file names & location
@@ -128,45 +45,62 @@ def draw_wtp_layout(G, comp_df, out_dir="", out_file="system_layout",
     else:
         output_path = out_dir
 
-    fn = out_file.split(os.extsep)[0]  # strip away file ext and add our own
-    sys_config_dot = os.path.join(output_path, fn + '.dot')
-    sys_config_img = os.path.join(output_path, fn + '.png')
+    fname = out_file.split(os.extsep)[0]  # strip away file ext and add our own
+    sys_config_dot = os.path.join(output_path, fname + '.dot')
+    sys_config_img = os.path.join(output_path, fname + '.png')
 
+    if orientation.upper() not in ['TB', 'LR', 'RL', 'BT']:
+        orientation = 'TB'
+
+    if connector_type.lower() not in ['spline', 'ortho', 'line',
+                                      'polyline', 'curved']:
+        connector_type = 'spline'
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     # Draw graph using pygraphviz, and define general node & edge attributes
 
     A = nx.nx_agraph.to_agraph(G)
 
-    A.graph_attr.update(resolution=200,
-                        directed=True,
-                        labelloc="t",
-                        label=graph_label,
-                        rankdir="TB",
-                        ranksep=1,
-                        splines="ortho",
-                        center="true",
-                        forcelabels="true",
-                        fontname="Helvetica-Bold",
-                        fontcolor="#444444",
-                        fontsize=24)
+    A.graph_attr.update(
+        resolution=200,
+        directed=True,
+        labelloc="t",
+        label='< <BR/>'+graph_label+'<BR/> >',
+        rankdir=orientation,
+        ranksep="1.0 equally",
+        splines=connector_type,
+        center="true",
+        forcelabels="true",
+        pad=0.2,
+        nodesep=0.4,
+        fontname="Helvetica-Bold",
+        fontcolor="#444444",
+        fontsize=26,
+        smoothing="graph_dist",
+        concentrate="true",
+    )
 
-    A.node_attr.update(shape="circle",
-                       style="rounded,filled",
-                       fixedsize="true",
-                       width=1.4,
-                       height=1.4,
-                       xlp="0, 0",
-                       color="gray14",
-                       fillcolor="white",
-                       fontcolor="gray14",
-                       penwidth=1.5,
-                       fontname="Helvetica-Bold",
-                       fontsize=17)
+    A.node_attr.update(
+        shape="circle",
+        style="rounded,filled",
+        fixedsize="true",
+        width=1.5,
+        height=1.5,
+        xlp="0, 0",
+        color="royalblue3",  # gray14
+        fillcolor="white",
+        fontcolor="royalblue3",  # gray14
+        penwidth=1.5,
+        fontname="Helvetica-Bold",
+        fontsize=18,
+    )
 
-    A.edge_attr.update(arrowhead="normal",
-                       arrowsize="1.0",
-                       color="gray12",
-                       penwidth=1.0)
+    A.edge_attr.update(
+        arrowhead="normal",
+        arrowsize="1.0",
+        style="bold",
+        color="royalblue2",  # gray12
+        penwidth=1.2,
+    )
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     # Customise nodes based on node type or defined clusters
@@ -176,59 +110,68 @@ def draw_wtp_layout(G, comp_df, out_dir="", out_file="system_layout",
         label_mod = segment_long_labels(node, delims=['_', ' '])
         A.get_node(node).attr['label'] = label_mod
 
-        if str(comp_df.ix[node]['node_cluster']) == 'Water Supply':
-            A.get_node(node).attr.update(
-                shape="rect",
-                style="rounded,filled",
-                fixedsize="true",
-                height=1.0,
-                width=2.4,
-                color="royalblue1",
-                fillcolor="royalblue1",
-                fontcolor="white"
-            )
-
-        if str(comp_df.ix[node]['node_cluster']) == 'Power Supply':
+        if str(comp_df.ix[node]['node_type']).lower() == 'supply':
             A.get_node(node).attr['label'] = \
-                segment_long_labels(node, chars=10, delims=['_', ' '])
+                segment_long_labels(node, chars=12, delims=['_', ' '])
             A.get_node(node).attr.update(
+                label=A.get_node(node).attr['label'],
                 shape="rect",
                 style="rounded,filled",
                 fixedsize="true",
-                height=1.0,
-                width=2.4,
-                color="indianred",
-                fillcolor="indianred",
-                fontcolor="white"
+                color="limegreen",
+                fillcolor="white",
+                fontcolor="limegreen",
+                penwidth=2.0,
+                height=1.1,
+                width=2.0,
             )
 
-        if str(comp_df.ix[node]['node_type']) == 'sink':
+        if str(comp_df.ix[node]['node_type']).lower() == 'sink':
             A.get_node(node).attr.update(
                 shape="doublecircle",
                 rank="sink",
                 penwidth=2.0,
-                color="royalblue2",
+                color="orangered",  # royalblue3
                 fillcolor="white",
-                fontcolor="royalblue3"
+                fontcolor="orangered",  # royalblue3
             )
 
-        if str(comp_df.ix[node]['node_type']) == 'dependency':
+        if str(comp_df.ix[node]['node_type']).lower() == 'dependency':
             A.get_node(node).attr.update(
                 shape="circle",
+                rank="dependency",
                 penwidth=3.5,
-                color="maroon",
+                color="orchid",
                 fillcolor="white",
-                fontcolor="maroon"
+                fontcolor="orchid"
             )
 
-    for cluster in pd.unique(comp_df['node_cluster'].values):
-        grp = comp_df[comp_df['node_cluster'] == cluster].index.values.tolist()
-        A.add_subgraph(grp, rank='same', rankdir='LR')
+    if clustering == True:
+        for cluster in pd.unique(comp_df['node_cluster'].values):
+            grp = comp_df[comp_df['node_cluster'] == cluster].\
+                index.values.tolist()
+            cluster = '_'.join(cluster.split())
+            if  cluster.lower() not in ['none', '']:
+                cluster_name = 'cluster_'+cluster
+                rank = 'same'
+            else:
+                cluster_name = ''
+                rank = ''
+            A.add_subgraph(
+                nbunch=grp,
+                name=cluster_name,
+                style='invis',
+                label='',
+                clusterrank='local',
+                rank=rank,
+            )
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     A.write(sys_config_dot)
     A.draw(sys_config_img, format='png', prog='dot')
+    A.draw(os.path.join(output_path, fname + '.svg'),
+           format='svg', prog='dot', args='-Gsize=11,8\! -Gdpi=300')
 
 # -----------------------------------------------------------------------------
 
@@ -247,7 +190,7 @@ def main():
     sc = Scenario(SETUPFILE)
     fc = FacilityObj(SETUPFILE)
     # Define input files, output location, scenario inputs
-    SYS_CONFIG_FILE = os.path.join(sc.input_path, fc.sys_config_file_name)
+    # SYS_CONFIG_FILE = os.path.join(sc.input_path, fc.sys_config_file_name)
 
     print("Initiating drawing network model schematic...")
     fc.network.network_setup(fc)
