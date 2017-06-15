93ca91e7d7be08f0b10796b8fc3b5f6b2ca5558f
Simon Knapp <sleepingwell@gmail.com>
2017-06-16 01:23:45 +1000

    Add a convenience script for spinning up an AWS machine.

--- build-sifra-box.sh
+++ build-sifra-box.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-sudo apt-get update
+# This can be run as root, as would happen if it is used as user-data when
+# spinning up a machine in AWS, or as another user. The next block sets prefixes
+# for running commands for each case.
+if [ "$(whoami)" == "root" ]; then
+    SUDO=
+    NOSUDO="sudo -i -u ubuntu"
+else
+    SUDO=sudo
+    NOSUDO=
+fi
+
+$SUDO apt-get update
+# should really say (except it is time consuming and can require user
+# interaction as it stands)...
+#$SUDO apt-get upgrade
 
 # install prerequisites
-sudo apt-get install -y \
+$SUDO apt-get install -y \
     linux-image-extra-$(uname -r) \
     linux-image-extra-virtual \
     apt-transport-https \
@@ -13,26 +27,28 @@ sudo apt-get install -y \
     software-properties-common
 
 # install docker
-curl -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add
-sudo add-apt-repository \
+curl -fsSL https://apt.dockerproject.org/gpg | $SUDO apt-key add
+$SUDO add-apt-repository \
        "deb https://apt.dockerproject.org/repo/ \
        ubuntu-$(lsb_release -cs) \
        main"
-sudo apt-get update
-sudo apt-get -y install docker-engine
+$SUDO apt-get update
+$SUDO apt-get -y install docker-engine
 
 # install docker-compose
-sudo bash -c "curl -L https://github.com/docker/compose/releases/download/1.11.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
-sudo chmod +x /usr/local/bin/docker-compose
+$SUDO bash -c "curl -L https://github.com/docker/compose/releases/download/1.11.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+$SUDO chmod +x /usr/local/bin/docker-compose
 
 # add ubuntu to the docker group to avoid constant sudoing
-sudo usermod -a -G docker ubuntu
+# note that if you log onto the box before this command is run, then it won't
+# take effect. You would need to log off and then on again to have it do so.
+$SUDO usermod -a -G docker ubuntu
 
 # clone master from sifra.
 # Of course, you would want to change the url of this once your ready to commit
 # something.
-git clone https://github.com/GeoscienceAustralia/sifra.git ~/sifra
+$NOSUDO git clone https://github.com/GeoscienceAustralia/sifra.git /home/ubuntu/sifra
 
 # get an awsome tmux configuration
-curl https://raw.githubusercontent.com/Sleepingwell/tmux-conf/master/tmux.conf > ~/.tmux.conf
+curl https://raw.githubusercontent.com/Sleepingwell/tmux-conf/master/tmux.conf | $NOSUDO tee /home/ubuntu/.tmux.conf
 
