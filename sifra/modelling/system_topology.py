1a4fe4456a11223311b8347b6c0e320fb07b3059
marufr <maruf.r@outlook.com>
2017-11-15 09:39:57 +1100

    Restructured code to use a new `SystemTopology` class.

--- sifra/modelling/system_topology.py
+++ sifra/modelling/system_topology.py
@@ -6,250 +6,228 @@ import re
 
 # -----------------------------------------------------------------------------
 
-def msplit(string, delims):
-    s = string
-    for d in delims:
-        rep = d + '\n'
-        s = rep.join(x for x in s.split(d))
-    return s
+class SystemTopology(object):
+
+    infrastructure = ""         # `IFSystem` object
+    scenario = ""               # `Scenario` object
+    gviz = ""                   # placeholder for a pygraphviz agraph
+    component_attr = {}         # Dict for system comp attributes
+
+    orientation = "LR"          # Orientation of graph - Graphviz option
+    connector_type = "spline"   # Connector appearance - Graphviz option
+    clustering = False          # Cluster based on defined `node_cluster`
+
+    out_dir = ""
+    out_file = "system_topology"
+    graph_label = "System Component Topology"
+
+    def __init__(self, infrastructure, scenario):
+        self.infrastructure = infrastructure
+        self.scenario = scenario
+
+        for comp_id in infrastructure.components.keys():
+            self.component_attr[comp_id] = \
+                vars(infrastructure.components[comp_id])
+
+        if infrastructure.system_class.lower() in \
+                ['potablewatertreatmentplant']:
+            self.out_dir=scenario.output_path
+            self.graph_label="Water Treatment Plant Component Topology"
+            self.orientation="TB"
+            self.connector_type="ortho"
+            self.clustering=True
+        else:
+            self.out_dir=scenario.output_path
+            self.graph_label="System Component Topology"
+            self.orientation="LR"
+            self.connector_type="spline"
+            self.clustering=False
 
 
-def segment_long_labels(string, maxlen=7, delims=[]):
-    if (not delims) and (len(string) > maxlen):
-        return "\n".join(re.findall("(?s).{,"+str(maxlen)+"}", string))[:-1]
+    def draw_sys_topology(self, viewcontext):
+        """
+        Draws the component configuration for a given infrastructure system.
+        :param G: ipython graph object
+        :param component_attr: dict of attributes of system components
+        :param out_dir: location for savings outputs
+        :param out_file: name of output file
+        :param graph_label: label for the graph topology image
+        :param orientation: orientation of the graph (default is top-to-bottom)
+        :param connector_type: line connector type. Must be one of the types
+            supported by Graphviz (i.e. 'spline', 'ortho', 'line', 'polyline',
+            'curved')
+        :param clustering: whether to create subgraphs based on `node_cluster`
+            designated for components
+        :return: saves a visual representation of the topology of system
+            components in multiple formats (png, svg, and dot)
+        """
 
-    elif len(string) > maxlen:
-        return msplit(string, delims)
+        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        # Set up output file names & location
 
-    else:
-        return string
+        if not self.out_dir.strip():
+            output_path = os.getcwd()
+        else:
+            output_path = self.out_dir
 
+        # strip away file ext and add our own
+        fname = self.out_file.split(os.extsep)[0]
 
-# -----------------------------------------------------------------------------
+        if self.orientation.upper() not in ['TB', 'LR', 'RL', 'BT']:
+            self.orientation = 'TB'
 
-def draw_sys_topology(G, component_attr,
-                      out_dir="",
-                      out_file="system_topology",
-                      graph_label="System Topology",
-                      orientation="TB",
-                      connector_type="spline",
-                      clustering=False,
-                      viewcontext=""):
-    """
-    Draws the component configuration for a given infrastructure system.
-    :param G: ipython graph object
-    :param component_attr: dict of attributes of system components
-    :param out_dir: location for savings outputs
-    :param out_file: name of output file
-    :param graph_label: label for the graph topology image
-    :param orientation: orientation of the graph (default is top-to-bottom)
-    :param connector_type: line connector type. Must be one of the types
-        supported by Graphviz (i.e. 'spline', 'ortho', 'line', 'polyline',
-        'curved')
-    :param clustering: whether to create subgraphs based on `node_cluster`
-        designated for components
-    :return: saves a visual representation of the topology of system
-        components in multiple formats (png, svg, and dot)
-    """
+        if self.connector_type.lower() not in \
+                ['spline', 'ortho', 'line', 'polyline', 'curved']:
+            self.connector_type = 'spline'
+        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        # Draw graph using pygraphviz, and define general node & edge attributes
+        G = self.infrastructure.component_graph.comp_graph
+        elist = G.get_edgelist()
+        named_elist = []
+        for t in elist:
+            named_elist.append((G.vs[t[0]]['name'],
+                                G.vs[t[1]]['name']))
+        A = nx.DiGraph(named_elist)
 
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    # Set up output file names & location
+        self.gviz = nx.nx_agraph.to_agraph(A)
 
-    if not out_dir.strip():
-        output_path = os.getcwd()
-    else:
-        output_path = out_dir
-
-    fname = out_file.split(os.extsep)[0]  # strip away file ext and add our own
-
-    if orientation.upper() not in ['TB', 'LR', 'RL', 'BT']:
-        orientation = 'TB'
-
-    if connector_type.lower() not in \
-            ['spline', 'ortho', 'line', 'polyline', 'curved']:
-        connector_type = 'spline'
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    # Draw graph using pygraphviz, and define general node & edge attributes
-
-    elist = G.get_edgelist()
-    named_elist = []
-    for t in elist:
-        named_elist.append((G.vs[t[0]]['name'],
-                            G.vs[t[1]]['name']))
-    A = nx.DiGraph(named_elist)
-    A = nx.nx_agraph.to_agraph(A)
-
-    A.graph_attr.update(
-        resolution=200,
-        directed=True,
-        labelloc="t",
-        label='< <BR/>'+graph_label+'<BR/> >',
-        rankdir=orientation,
-        ranksep="1.0 equally",
-        splines=connector_type,
-        center="true",
-        forcelabels="true",
-        pad=0.2,
-        nodesep=0.4,
-        fontname="Helvetica-Bold",
-        fontcolor="#444444",
-        fontsize=26,
-        smoothing="graph_dist",
-        concentrate="true",
-    )
-
-    A.node_attr.update(
-        shape="circle",
-        style="rounded,filled",
-        fixedsize="true",
-        width=1.8,
-        height=1.8,
-        xlp="0, 0",
-        color="royalblue3",  # gray14
-        fillcolor="white",
-        fontcolor="royalblue3",  # gray14
-        penwidth=1.5,
-        fontname="Helvetica-Bold",
-        fontsize=18,
-    )
-
-    A.edge_attr.update(
-        arrowhead="normal",
-        arrowsize="1.0",
-        style="bold",
-        color="royalblue2",  # gray12
-        penwidth=1.2,
-    )
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    # Customise nodes based on node type or defined clusters
-
-    for node in component_attr.keys():
-
-        label_mod = segment_long_labels(node, delims=['_', ' '])
-        A.get_node(node).attr['label'] = label_mod
-
-        if str(component_attr[node]['node_type']).lower() == 'supply':
-            A.get_node(node).attr['label'] = \
-                segment_long_labels(node, maxlen=12, delims=['_', ' '])
-            A.get_node(node).attr.update(
-                label=A.get_node(node).attr['label'],
-                shape="rect",
-                style="rounded,filled",
-                fixedsize="true",
-                color="limegreen",
-                fillcolor="white",
-                fontcolor="limegreen",
-                penwidth=2.0,
-                height=1.2,
-                width=2.2,
-            )
-
-        if str(component_attr[node]['node_type']).lower() == 'sink':
-            A.get_node(node).attr.update(
-                shape="doublecircle",
-                rank="sink",
-                penwidth=2.0,
-                color="orangered",  # royalblue3
-                fillcolor="white",
-                fontcolor="orangered",  # royalblue3
-            )
-
-        if str(component_attr[node]['node_type']).lower() == 'dependency':
-            A.get_node(node).attr.update(
-                shape="circle",
-                rank="dependency",
-                penwidth=3.5,
-                color="orchid",
-                fillcolor="white",
-                fontcolor="orchid"
-            )
-
-    node_clusters = list(set([component_attr[id]['node_cluster']
-                              for id in component_attr.keys()]))
-    if clustering == True:
-        for cluster in node_clusters:
-            grp = [k for k in component_attr.keys()
-                   if component_attr[k]['node_cluster']==cluster]
-            cluster = '_'.join(cluster.split())
-            if  cluster.lower() not in ['none', '']:
-                cluster_name = 'cluster_'+cluster
-                rank = 'same'
-            else:
-                cluster_name = ''
-                rank = ''
-            A.add_subgraph(
-                nbunch=grp,
-                name=cluster_name,
-                style='invis',
-                label='',
-                clusterrank='local',
-                rank=rank,
-            )
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    if viewcontext == "as-built":
-        A.write(os.path.join(output_path, fname + '.dot'))
-        A.draw(os.path.join(output_path, fname + '.png'),
-               format='png', prog='dot')
-
-    A.draw(os.path.join(output_path, fname + '.svg'),
-           format='svg', prog='dot', args='-Gsize=11,8\! -Gdpi=300')
-
-
-# -----------------------------------------------------------------------------
-
-def set_sys_topology_view(infrastructure, scenario, viewcontext="as-built"):
-    component_attr = {}
-    for comp_id in infrastructure.components.keys():
-        component_attr[comp_id] = vars(infrastructure.components[comp_id])
-
-    if infrastructure.system_class.lower() in ['potablewatertreatmentplant']:
-        draw_sys_topology(
-            infrastructure.component_graph.comp_graph,
-            component_attr,
-            out_dir=scenario.output_path,
-            graph_label="Water Treatment Plant Component Topology",
-            orientation="TB",
-            connector_type="ortho",
-            clustering=True,
-            viewcontext=viewcontext
-        )
-    else:
-        draw_sys_topology(
-            infrastructure.component_graph.comp_graph,
-            component_attr,
-            out_dir=scenario.output_path,
-            graph_label="System Component Topology",
-            orientation="LR",
-            connector_type="spline",
-            clustering=False,
-            viewcontext = viewcontext
+        self.gviz.graph_attr.update(
+            resolution=200,
+            directed=True,
+            labelloc="t",
+            label='< <BR/>'+self.graph_label+'<BR/> >',
+            rankdir=self.orientation,
+            ranksep="1.0 equally",
+            splines=self.connector_type,
+            center="true",
+            forcelabels="true",
+            pad=0.2,
+            nodesep=0.4,
+            fontname="Helvetica-Bold",
+            fontcolor="#444444",
+            fontsize=26,
+            smoothing="graph_dist",
+            concentrate="true",
         )
 
+        self.gviz.node_attr.update(
+            shape="circle",
+            style="rounded,filled",
+            fixedsize="true",
+            width=1.8,
+            height=1.8,
+            xlp="0, 0",
+            color="royalblue3",  # gray14
+            fillcolor="white",
+            fontcolor="royalblue3",  # gray14
+            penwidth=1.5,
+            fontname="Helvetica-Bold",
+            fontsize=18,
+        )
 
-def main():
+        self.gviz.edge_attr.update(
+            arrowhead="normal",
+            arrowsize="1.0",
+            style="bold",
+            color="royalblue2",  # gray12
+            penwidth=1.2,
+        )
 
-    from sifraclasses import Scenario, PowerStation, PotableWaterTreatmentPlant
+        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        # Customise nodes based on node type or defined clusters
 
-    SETUPFILE = sys.argv[1]
-    discard = {}
-    config = {}
+        for node in self.component_attr.keys():
+            label_mod = self.segment_long_labels(node, delims=['_', ' '])
+            self.gviz.get_node(node).attr['label'] = label_mod
 
-    exec (open(SETUPFILE).read(), discard, config)
+            if str(self.component_attr[node]['node_type']).lower() \
+                    == 'supply':
+                self.gviz.get_node(node).attr['label'] = \
+                    self.segment_long_labels(node, maxlen=12,
+                                             delims=['_', ' '])
+                self.gviz.get_node(node).attr.update(
+                    label=self.gviz.get_node(node).attr['label'],
+                    shape="rect",
+                    style="rounded,filled",
+                    fixedsize="true",
+                    color="limegreen",
+                    fillcolor="white",
+                    fontcolor="limegreen",
+                    penwidth=2.0,
+                    height=1.2,
+                    width=2.2,
+                )
 
-    print("Setting up objects...")
-    FacilityObj = eval(config["SYSTEM_CLASS"])
-    sc = Scenario(SETUPFILE)
-    fc = FacilityObj(SETUPFILE)
+            if str(self.component_attr[node]['node_type']).lower() \
+                    == 'sink':
+                self.gviz.get_node(node).attr.update(
+                    shape="doublecircle",
+                    rank="sink",
+                    penwidth=2.0,
+                    color="orangered",  # royalblue3
+                    fillcolor="white",
+                    fontcolor="orangered",  # royalblue3
+                )
 
-    print("Initiating drawing network model schematic...")
-    fc.network.network_setup(fc)
-    print("Drawing complete.\n")
+            if str(self.component_attr[node]['node_type']).lower() \
+                    == 'dependency':
+                self.gviz.get_node(node).attr.update(
+                    shape="circle",
+                    rank="dependency",
+                    penwidth=3.5,
+                    color="orchid",
+                    fillcolor="white",
+                    fontcolor="orchid"
+                )
 
-if __name__=="__main__":
-    main()
+        node_clusters = list(set([self.component_attr[id]['node_cluster']
+                                  for id in self.component_attr.keys()]))
+        if self.clustering == True:
+            for cluster in node_clusters:
+                grp = [k for k in self.component_attr.keys()
+                       if self.component_attr[k]['node_cluster']==cluster]
+                cluster = '_'.join(cluster.split())
+                if  cluster.lower() not in ['none', '']:
+                    cluster_name = 'cluster_'+cluster
+                    rank = 'same'
+                else:
+                    cluster_name = ''
+                    rank = ''
+                self.gviz.add_subgraph(
+                    nbunch=grp,
+                    name=cluster_name,
+                    style='invis',
+                    label='',
+                    clusterrank='local',
+                    rank=rank,
+                )
+
+        if viewcontext == "as-built":
+            self.gviz.write(os.path.join(output_path, fname + '.dot'))
+            self.gviz.draw(os.path.join(output_path, fname + '.png'),
+                   format='png', prog='dot')
+
+            self.gviz.draw(os.path.join(output_path, fname + '.svg'),
+               format='svg', prog='dot', args='-Gsize=11,8\! -Gdpi=300')
+        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+    def msplit(self, string, delims):
+        s = string
+        for d in delims:
+            rep = d + '\n'
+            s = rep.join(x for x in s.split(d))
+        return s
+
+    def segment_long_labels(self, string, maxlen=7, delims=[]):
+        if (not delims) and (len(string) > maxlen):
+            return "\n".join(
+                re.findall("(?s).{," + str(maxlen) + "}", string))[:-1]
+        elif len(string) > maxlen:
+            return self.msplit(string, delims)
+        else:
+            return string
+
 
 
 '''
