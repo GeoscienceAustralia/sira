03fec3085876a91fd37dea6cc507df96d9c6b136
marufr <maruf.r@outlook.com>
2018-02-15 17:30:20 +1100

    Documentation updated

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -164,7 +164,6 @@ html_theme_options = {
     'logo_only': False,
     'display_version': True,
     'prev_next_buttons_location': None,
-    'style_external_links': True,
     # Toc options
     'collapse_navigation': False,
     'sticky_navigation': True,
