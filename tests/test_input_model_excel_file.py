84f09945c2f0fa5a57d81dbbaff8f34d9e3fd7e0
marufr <maruf.r@outlook.com>
2018-04-04 09:49:18 +1000

    Fixed bugs in pandas dataframe header referencing that were causing test to fail.

--- tests/test_input_model_excel_file.py
+++ tests/test_input_model_excel_file.py
@@ -19,22 +19,43 @@ class TestReadingExcelFile(ut.TestCase):
                     if ".xlsx" in file_name:
                         self.model_xlsx_files.append(os.path.join(root, file_name))
 
-        self.required_sheets = ['component_list', 'component_connections', 'supply_setup', 'output_setup',
-                                'comp_type_dmg_algo', 'damage_state_def']
+        self.required_sheets = ['component_list',
+                                'component_connections',
+                                'supply_setup',
+                                'output_setup',
+                                'comp_type_dmg_algo',
+                                'damage_state_def']
 
     def test_folder_structure(self):
 
-        self.assertTrue(os.path.isdir(os.path.join(self.project_root_dir, "models")), "core models folder not found at " + self.project_root_dir + "!")
-        self.assertTrue(os.path.isdir(os.path.join(self.project_root_dir, "tests", "models")), "test models folder not found at " + self.project_root_dir + "!")
+        self.assertTrue(
+            os.path.isdir(os.path.join(self.project_root_dir, "models")),
+            "core models folder not found at " + self.project_root_dir + "!"
+        )
 
-        self.assertTrue(os.path.isdir(os.path.join(self.project_root_dir, "simulation_setup")), "core simulation setup folder not found at " + self.project_root_dir + "!")
-        self.assertTrue(os.path.isdir(os.path.join(self.project_root_dir, "tests",  "simulation_setup")),"test simulation setup folder not found at " + self.project_root_dir + "!")
+        self.assertTrue(
+            os.path.isdir(os.path.join(self.project_root_dir, "tests", "models")),
+            "test models folder not found at " + self.project_root_dir + "!"
+        )
+
+        self.assertTrue(
+            os.path.isdir(os.path.join(self.project_root_dir, "simulation_setup")),
+            "core simulation setup folder not found at " + self.project_root_dir + "!"
+        )
+
+        self.assertTrue(
+            os.path.isdir(os.path.join(self.project_root_dir, "tests",  "simulation_setup")),
+            "test simulation setup folder not found at " + self.project_root_dir + "!"
+        )
 
 
     def test_model_files_exists(self):
 
         for model_file in self.model_xlsx_files:
-            self.assertTrue(os.path.isfile(model_file), "Model excel file not found on path  at" + model_file + " !")
+            self.assertTrue(
+                os.path.isfile(model_file),
+                "Model excel file not found on path  at" + model_file + " !"
+            )
 
     def test_required_sheets_exist(self):
 
@@ -48,8 +69,11 @@ class TestReadingExcelFile(ut.TestCase):
     def test_reading_data_from_component_list(self):
 
         for model_file in self.model_xlsx_files:
-            component_list = pd.read_excel(model_file, sheet_name='component_list', index_col='component_id',
-                                           header=0, skiprows=0, skipinitialspace=True)
+            component_list = pd.read_excel(model_file,
+                                           sheet_name='component_list',
+                                           header=0, skiprows=0,
+                                           index_col=0,
+                                           skipinitialspace=True)
 
             self.assertTrue(isinstance(len(component_list.index.tolist()), int))
 
@@ -58,8 +82,11 @@ class TestReadingExcelFile(ut.TestCase):
         required_col_names = ['origin', 'destination', 'link_capacity', 'weight']
 
         for model_file in self.model_xlsx_files:
-            component_connections = pd.read_excel(model_file, sheet_name='component_connections', index_col=None,
-                                                  header=0, skiprows=0, skipinitialspace=True)
+            component_connections = pd.read_excel(model_file,
+                                                  sheet_name='component_connections',
+                                                  index_col=None,
+                                                  header=0, skiprows=0,
+                                                  skipinitialspace=True)
 
             self.assertTrue(set(required_col_names) <= set(component_connections.columns.values.tolist()),
                             "Required column name not found!")
@@ -71,7 +98,7 @@ class TestReadingExcelFile(ut.TestCase):
                 self.assertTrue(isinstance(float(connection_values['weight']), float))
                 self.assertTrue(isinstance((connection_values['destination']), unicode or str))
 
-    def test_reading_data_from_supply_setupt(self):
+    def test_reading_data_from_supply_setup(self):
 
         # index coloum ingnored : 'input_node'
         required_col_names = ['input_capacity', 'capacity_fraction', 'commodity_type']
@@ -94,11 +121,17 @@ class TestReadingExcelFile(ut.TestCase):
 
     def test_reading_data_from_output_setup(self):
         # index coloum ingnored : 'output_node'
-        required_col_names = ['production_node', 'output_node_capacity', 'capacity_fraction', 'priority']
+        required_col_names = ['production_node',
+                              'output_node_capacity',
+                              'capacity_fraction',
+                              'priority']
 
         for model_file in self.model_xlsx_files:
-            output_setup = pd.read_excel(model_file, sheet_name='output_setup', index_col='output_node', header=0,
-                                         skiprows=0, skipinitialspace=True)
+            output_setup = pd.read_excel(model_file,
+                                         sheet_name='output_setup',
+                                         header=0,
+                                         skiprows=0,
+                                         skipinitialspace=True)
 
             self.assertTrue(set(required_col_names) <= set(output_setup.columns.tolist()),
                             "Required column name not found!" + '\n' +
@@ -109,23 +142,30 @@ class TestReadingExcelFile(ut.TestCase):
             self.assertTrue(output_setup['output_node_capacity'].sum() > 0)
 
             for index, output_values in output_setup.iterrows():
-
-                self.assertTrue(isinstance((str(index[0])), str))
                 self.assertTrue(isinstance((output_values['production_node']), unicode or str))
                 self.assertTrue(isinstance((float(output_values['output_node_capacity'])), float))
                 self.assertTrue(isinstance((float(output_values['capacity_fraction'])), float))
                 self.assertTrue(isinstance(int(output_values['priority']), int))
 
 
-
     def test_reading_data_from_comp_type_dmg_algo(self):
 
         # there can be arbitrary number of coloums to supply parameters for specific functions
-        required_col_names = ['is_piecewise', 'damage_function', 'damage_ratio', 'functionality', 'recovery_mean', 'recovery_std', 'recovery_95percentile']
+        required_col_names = ['is_piecewise',
+                              'damage_function',
+                              'damage_ratio',
+                              'functionality',
+                              'recovery_mean',
+                              'recovery_std',
+                              'recovery_95percentile']
 
         for model_file in self.model_xlsx_files:
-            comp_type_dmg_algo = pd.read_excel(model_file, sheet_name='comp_type_dmg_algo', index_col=[0, 1, 2],
-                                               header=0, skiprows=0, skipinitialspace=True)
+            comp_type_dmg_algo = pd.read_excel(model_file,
+                                               sheet_name='comp_type_dmg_algo',
+                                               index_col=[0, 1, 2],
+                                               header=0,
+                                               skiprows=0,
+                                               skipinitialspace=True)
 
             self.assertTrue(set(required_col_names) <= set(comp_type_dmg_algo.columns.tolist()),
                             "Required column name not found!" + '\n' +
@@ -134,7 +174,15 @@ class TestReadingExcelFile(ut.TestCase):
                             "file name : " + model_file)
 
             # current implemented function
-            possible_values_of_damage_function = ["StepFunc", "LogNormalCDF", "Lognormal", "NormalCDF", "ConstantFunction", "Level0Response", "Level0Recovery", "PiecewiseFunction", "RecoveryFunction"]
+            possible_values_of_damage_function = ["StepFunc",
+                                                  "LogNormalCDF",
+                                                  "Lognormal",
+                                                  "NormalCDF",
+                                                  "ConstantFunction",
+                                                  "Level0Response",
+                                                  "Level0Recovery",
+                                                  "PiecewiseFunction",
+                                                  "RecoveryFunction"]
 
             for index, damage_state in comp_type_dmg_algo.iterrows():
                 # id
@@ -144,11 +192,16 @@ class TestReadingExcelFile(ut.TestCase):
                 # damage_state
                 self.assertTrue(isinstance((index[2]), unicode or str))
 
-                self.assertTrue(str(damage_state['damage_function']) in set(possible_values_of_damage_function),
-                                "Required damage_function name not found!" + '\n' +
-                                "damage_function expected names: " + str(possible_values_of_damage_function) + '\n' +
-                                "damage_function name supplied: " + str(damage_state['damage_function']) + '\n' +
-                                "file name : " + model_file)
+                self.assertTrue(
+                    str(damage_state['damage_function']) in
+                        set(possible_values_of_damage_function),
+                    "Required damage_function name not found!" + '\n' +
+                    "damage_function expected names: " +
+                    str(possible_values_of_damage_function) + '\n' +
+                    "damage_function name supplied: " +
+                    str(damage_state['damage_function']) + '\n' +
+                    "file name : " + model_file
+                )
 
                 self.assertTrue(isinstance(damage_state['is_piecewise'],  unicode or str))
                 self.assertTrue(isinstance(damage_state['damage_function'], unicode or str))
@@ -163,8 +216,12 @@ class TestReadingExcelFile(ut.TestCase):
 
     def test_reading_data_from_damage_state_def(self):
         for model_file in self.model_xlsx_files:
-            damage_state_def = pd.read_excel(model_file, sheet_name='damage_state_def', index_col=[0, 1],
-                                                  header=0, skiprows=0, skipinitialspace=True)
+            damage_state_def = pd.read_excel(model_file,
+                                             sheet_name='damage_state_def',
+                                             index_col=[0, 1],
+                                             header=0,
+                                             skiprows=0,
+                                             skipinitialspace=True)
 
             for index, damage_def in damage_state_def.iterrows():
                 self.assertTrue(isinstance(index[0], unicode or str), type(index[0]))
