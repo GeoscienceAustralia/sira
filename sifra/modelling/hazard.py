ef40b14bf2f55f6795bc42904533814f8c321d16
marufr <maruf.r@outlook.com>
2018-04-24 10:04:36 +1000

    PEP8 formatting updates.

--- sifra/modelling/hazard.py
+++ sifra/modelling/hazard.py
@@ -17,8 +17,10 @@ class Hazard(object):
 
         for comp in self.scenario_hazard_data:
 
-            if round(float(comp["longitude"]), self.round_off) == round(float(longitude), self.round_off):
-                if round(float(comp["latitude"]), self.round_off) == round(float(latitude), self.round_off):
+            if round(float(comp["longitude"]), self.round_off) \
+                    == round(float(longitude), self.round_off):
+                if round(float(comp["latitude"]), self.round_off) \
+                        == round(float(latitude), self.round_off):
                     return comp["hazard_intensity"]
 
         raise Exception("Invalid Values for Longitude or Latitude")
@@ -34,7 +36,10 @@ class Hazard(object):
 
         output = self.hazard_scenario_name+'\n'
         for hazrd in self.scenario_hazard_data:
-            output = output + "longitude: "+str(hazrd["longitude"]) + " latitude: " + str(hazrd["latitude"]) +" hazard_intensity: "+ str(hazrd["hazard_intensity"]) +'\n'
+            output = output + \
+                     "longitude: "+str(hazrd["longitude"]) + \
+                     " latitude: " + str(hazrd["latitude"]) + \
+                     " hazard_intensity: "+ str(hazrd["hazard_intensity"]) +'\n'
         return output
 
 class HazardsContainer(object):
@@ -53,7 +58,8 @@ class HazardsContainer(object):
         # get hazard data from scenario file
         if configuration.HAZARD_INPUT_METHOD == "scenario_file":
             self.scenario_hazard_data, self.hazard_scenario_list = \
-                HazardsContainer.populate_scenario_hazard_data_using_hazard_file(configuration.SCENARIO_FILE)
+                HazardsContainer.populate_scenario_hazard_using_hazard_file(
+                    configuration.SCENARIO_FILE)
 
             self.num_hazard_pts = len(self.hazard_scenario_list)
 
@@ -61,20 +67,31 @@ class HazardsContainer(object):
         elif configuration.HAZARD_INPUT_METHOD == "hazard_array":
 
             self.num_hazard_pts = \
-                int(round((configuration.INTENSITY_MEASURE_MAX - configuration.INTENSITY_MEASURE_MIN) /
-                          float(configuration.INTENSITY_MEASURE_STEP) + 1))
+                int(round((configuration.INTENSITY_MEASURE_MAX
+                           - configuration.INTENSITY_MEASURE_MIN) /
+                          float(configuration.INTENSITY_MEASURE_STEP) + 1
+                          )
+                    )
 
-            # using the limits and step generate a list of hazard intensity values
-            self.hazard_scenario_list = np.linspace(configuration.INTENSITY_MEASURE_MIN,
-                                                    configuration.INTENSITY_MEASURE_MAX,
-                                                    num=self.num_hazard_pts)
+            # Using the limits and step generate a list of hazard
+            # intensity values
+            self.hazard_scenario_list \
+                = np.linspace(configuration.INTENSITY_MEASURE_MIN,
+                              configuration.INTENSITY_MEASURE_MAX,
+                              num=self.num_hazard_pts)
 
             # containing hazard value for each location
             self.scenario_hazard_data, self.hazard_scenario_list = \
-                HazardsContainer.populate_scenario_hazard_data_using_hazard_array(self.hazard_scenario_list)
+                HazardsContainer.populate_scenario_hazard_using_hazard_array(
+                    self.hazard_scenario_list)
 
         for hazard_scenario_name in self.scenario_hazard_data.keys():
-            self.listOfhazards.append(Hazard(hazard_scenario_name, self.scenario_hazard_data[hazard_scenario_name]))
+            self.listOfhazards.append(
+                Hazard(
+                    hazard_scenario_name,
+                    self.scenario_hazard_data[hazard_scenario_name]
+                    )
+                )
 
         self.hazard_scenario_name = self.hazard_scenario_list
 
@@ -83,7 +100,7 @@ class HazardsContainer(object):
             yield hazard_intensity
 
     @staticmethod
-    def populate_scenario_hazard_data_using_hazard_file(scenario_file):
+    def populate_scenario_hazard_using_hazard_file(scenario_file):
         root = os.path.dirname(os.path.abspath(__file__))
         csv_path = os.path.join(root, "hazard", scenario_file )
         scenario_hazard_data = {}
@@ -91,8 +108,9 @@ class HazardsContainer(object):
         with open(csv_path, "rb") as f_obj:
             reader = csv.DictReader(f_obj, delimiter=',')
 
-            hazard_scenario_list = [scenario for scenario in reader.fieldnames if
-                                    scenario not in ["longitude", "latitude"]]
+            hazard_scenario_list \
+                = [scenario for scenario in reader.fieldnames if
+                   scenario not in ["longitude", "latitude"]]
 
             for scenario in hazard_scenario_list:
                 scenario_hazard_data[scenario] = []
@@ -102,20 +120,23 @@ class HazardsContainer(object):
                     if col not in ["longitude", "latitude"]:
                         hazard_intensity = row[col]
                         scenario_hazard_data[col].append(
-                            {"longitude": row["longitude"], "latitude": row["latitude"],
+                            {"longitude": row["longitude"],
+                             "latitude": row["latitude"],
                              "hazard_intensity": hazard_intensity})
 
         return scenario_hazard_data, hazard_scenario_list
 
     @staticmethod
-    def populate_scenario_hazard_data_using_hazard_array(num_hazard_pts):
+    def populate_scenario_hazard_using_hazard_array(num_hazard_pts):
 
         scenario_hazard_data = OrderedDict()
         hazard_scenario_list = []
         for i, hazard_intensity in enumerate(num_hazard_pts):
             hazard_scenario_list.append("s_"+str(i))
-            scenario_hazard_data["s_"+str(i)] = [{"longitude": 0, "latitude": 0, "hazard_intensity": hazard_intensity}]
+            scenario_hazard_data["s_"+str(i)] \
+                = [{"longitude": 0,
+                    "latitude": 0,
+                    "hazard_intensity": hazard_intensity}]
 
         return scenario_hazard_data, hazard_scenario_list
-
 
