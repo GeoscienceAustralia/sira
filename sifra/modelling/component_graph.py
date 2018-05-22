b23e8d901624f284f729209824835efcb8d78bf2
marufr <maruf.r@outlook.com>
2018-05-22 11:33:21 +1000

    Refactored attribute `comp_graph` to 'digraph' to avoid naming confusion

--- sifra/modelling/component_graph.py
+++ sifra/modelling/component_graph.py
@@ -14,7 +14,7 @@ class ComponentGraph(object):
         :param comp_sample_func: Array of the functionality of each component (1.0 -> 0.0).
         """
         # create the directed graph
-        self.comp_graph = igraph.Graph(directed=True)
+        self.digraph = igraph.Graph(directed=True)
 
         # if we don't have a functionality array create a default one with 1.0's
         if comp_sample_func is None:
@@ -30,9 +30,9 @@ class ComponentGraph(object):
             component = components[comp_id]
             comp_index = id_index_map[comp_id]
             # check that the component is not already in the graph
-            if len(self.comp_graph.vs) == 0 or comp_id not in self.comp_graph.vs['name']:
+            if len(self.digraph.vs) == 0 or comp_id not in self.digraph.vs['name']:
                 # add new component
-                self.comp_graph.add_vertex(name=comp_id)
+                self.digraph.add_vertex(name=comp_id)
 
             # iterate through this components connected components
             for dest_comp_id, destination_component in component.destination_components.items():
@@ -44,14 +44,14 @@ class ComponentGraph(object):
                     self.update_dependency(comp_sample_func, comp_index, dest_index)
 
                 # Is the child node in the graph
-                if len(self.comp_graph.vs) == 0 or dest_comp_id not in self.comp_graph.vs['name']:
+                if len(self.digraph.vs) == 0 or dest_comp_id not in self.digraph.vs['name']:
                     # add new child component
-                    self.comp_graph.add_vertex(name=dest_comp_id)
+                    self.digraph.add_vertex(name=dest_comp_id)
 
                 # connect the parent and child vertices with an edge.
                 # The functionality of the parent vertice is the value of the edge capacity
-                self.comp_graph.add_edge(comp_id, dest_comp_id,
-                                         capacity=comp_sample_func[comp_index])
+                self.digraph.add_edge(comp_id, dest_comp_id,
+                                      capacity=comp_sample_func[comp_index])
 
     def update_capacity(self, components, comp_sample_func):
         """Update the graph to change the edge's capacity value to
@@ -74,9 +74,9 @@ class ComponentGraph(object):
                     self.update_dependency(comp_sample_func, comp_index, dest_index)
 
                 # Determine the edge id from the id's of the parent and child node
-                edge_id = self.comp_graph.get_eid(comp_id, dest_comp_id)
+                edge_id = self.digraph.get_eid(comp_id, dest_comp_id)
                 # update the edge dict with the functionality of the parent vertice
-                self.comp_graph.es[edge_id]['capacity'] = comp_sample_func[comp_index]
+                self.digraph.es[edge_id]['capacity'] = comp_sample_func[comp_index]
 
     def update_dependency(self,comp_sample_func, parent, dependent):
         min_capacity = min(comp_sample_func[parent], comp_sample_func[dependent])
@@ -91,7 +91,7 @@ class ComponentGraph(object):
         graph, which must implement the igraph methods."""
 
         # Use the parameter graph if provided
-        comp_graph = external if external else self.comp_graph
+        comp_graph = external if external else self.digraph
         # Iterate through the edges
         for edge in comp_graph.get_edgelist():
             # we need to use the two vertice id's to get the edge id
@@ -102,9 +102,9 @@ class ComponentGraph(object):
     def maxflow(self, supply_comp_id, output_comp_id):
         """Computes the maximum flow between two nodes."""
         # determine the vertice id's for the two components
-        sup_v = self.comp_graph.vs.find(supply_comp_id)
-        out_v = self.comp_graph.vs.find(output_comp_id)
+        sup_v = self.digraph.vs.find(supply_comp_id)
+        out_v = self.digraph.vs.find(output_comp_id)
         # calculate the maximum flow value between the two id's
-        return self.comp_graph.maxflow_value(sup_v.index,
-                                             out_v.index,
-                                             self.comp_graph.es['capacity'])
+        return self.digraph.maxflow_value(sup_v.index,
+                                          out_v.index,
+                                          self.digraph.es['capacity'])
