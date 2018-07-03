d4bcd9b7a927e9ad27d745161a10f00ae1b8a414
marufr <maruf.r@outlook.com>
2018-07-03 10:40:33 +1000

    Cleaned up `setup` file.

--- setup.py
+++ setup.py
@@ -1,29 +1,26 @@
-from setuptools import setup, find_packages
-from itertools import ifilter
-from os import path
-from ast import parse
+import setuptools
+import sifra
 
+PACKAGE_NAME = 'sifra'
 DESCRIPTION = 'SIFRA: System for Infrastructure Facility Resilience Analysis'
-MAINTAINER = 'Maruf Rahman'
-CONTRIBUTORS = ['Maruf Rahman', 'Sudipta Basak', 'Sheece Gardezi']
-MAINTAINER_EMAIL = 'maruf.rahman@ga.gov.au'
 URL = 'https://github.com/GeoscienceAustralia/sifra'
 LICENSE = 'Apache v2.0'
 
-if __name__ == '__main__':
-    package_name = 'sifra'
-    with open(path.join(package_name, '__init__.py')) as f:
-        __version__ = parse(next(ifilter(lambda line: line.startswith('__version__'), f))).body[0].value.s
+MAINTAINER = 'Maruf Rahman'
+CONTRIBUTORS = ['Maruf Rahman', 'Sudipta Basak', 'Sheece Gardezi']
+MAINTAINER_EMAIL = 'maruf.rahman@ga.gov.au'
 
-    setup(
-        name=package_name,
-        version=__version__,
-        url=URL,
-        license=LICENSE,
-        author=MAINTAINER,
-        test_suite='.tests',
-        tests_require=['nose'],
-        install_requires=['graphviz'],
-        packages=find_packages(),
-        package_dir={package_name: package_name}
+if __name__ == '__main__':
+    setuptools.setup(
+    name=PACKAGE_NAME,
+    version=sifra.__version__,
+    description=DESCRIPTION,
+    url=URL,
+    license=LICENSE,
+    author=MAINTAINER,
+    test_suite='.tests',
+    tests_require=['nose'],
+    install_requires=['graphviz'],
+    packages=setuptools.find_packages(),
+    package_dir={PACKAGE_NAME: PACKAGE_NAME}
     )
