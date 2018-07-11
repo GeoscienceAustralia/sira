5eb204c6ebee1eeadb5acc71ca21c486e061ea70
marufr <maruf.r@outlook.com>
2018-07-11 15:37:57 +1000

    Defined `__all__` for * imports.

--- sifra/__init__.py
+++ sifra/__init__.py
@@ -8,3 +8,14 @@ from sifra.__about__ import (
     __license__,
     __copyright__
 )
+
+__all__ = [
+    __packagename__,
+    __description__,
+    __url__,
+    __version__,
+    __author__,
+    __email__,
+    __license__,
+    __copyright__
+]
