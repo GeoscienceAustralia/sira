7be8c1abc78e20b3190720c74abc9027574e5ed8
marufr <maruf.r@outlook.com>
2018-08-27 07:38:25 +1000

    Updated version to 0.2.2

--- sifra/__about__.py
+++ sifra/__about__.py
@@ -2,7 +2,7 @@ __packagename__ = "sifra"
 __description__ = "System for Infrastructure Facility Resilience Analysis"
 __url__ = "https://github.com/GeoscienceAustralia/sifra"
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 __author__ = "Geoscience Australia"
 __email__ = "maruf.rahman@ga.gov.au"
