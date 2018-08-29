73a7c822ea3280a251dca01a20c333c1243b4d83
marufr <maruf.r@outlook.com>
2018-08-29 12:45:58 +0800

    Damage scale boundary changes for experiments.

--- sifra/modelling/infrastructure.py
+++ sifra/modelling/infrastructure.py
@@ -306,7 +306,7 @@ class Infrastructure(Base):
         :return:  Array of real numbers representing damage state boundaries
         """
         # todo introduce system subclass to infrastructure
-        return [0.01, 0.15, 0.4, 0.8, 1.0]
+        return [0.05, 0.2, 0.5, 0.8, 1.0]
 
     def get_component_class_list(self):
         """
