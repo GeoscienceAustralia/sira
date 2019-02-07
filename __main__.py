936f066f2ec883be0115b5ddf8dd26ff2705bd7d
marufr <maruf.r@outlook.com>
2019-02-07 17:31:03 +1100

    Logging updated. Formatting for PEP8.

--- __main__.py
+++ __main__.py
@@ -28,6 +28,7 @@ init()
 import os
 import argparse
 from sifra.logger import rootLogger
+
 from sifra.configuration import Configuration
 from sifra.scenario import Scenario
 from sifra.modelling.hazard import HazardsContainer
@@ -48,15 +49,19 @@ import numpy as np
 def main():
 
     # define arg parser
-    parser = argparse.ArgumentParser(prog='sifra', description="run sifra", add_help=True)
+    parser = argparse.ArgumentParser(
+        prog='sifra', description="run sifra", add_help=True)
 
     parser.add_argument("-c", "--config_file", type=str)
     parser.add_argument("-m", "--model_file", type=str)
     parser.add_argument("-d", "--input_directory", type=str)
 
-    parser.add_argument("-s", "--simulation", action='store_true', default=False)
-    parser.add_argument("-f", "--fit", action='store_true', default=False)
-    parser.add_argument("-l", "--loss_analysis", action='store_true', default=False)
+    parser.add_argument("-s", "--simulation",
+                        action='store_true', default=False)
+    parser.add_argument("-f", "--fit",
+                        action='store_true', default=False)
+    parser.add_argument("-l", "--loss_analysis",
+                        action='store_true', default=False)
 
     parser.add_argument("-v", "--verbose", type=str,
                         help="Choose option for logging level from: \n"
@@ -66,40 +71,48 @@ def main():
 
     # error handling
     if args.input_directory and (args.config_file or args.model_file):
-        parser.error("--input_directory and [--config_file and --model_file] are mutually exclusive ...")
+        parser.error("--input_directory and [--config_file and --model_file]"
+                     " are mutually exclusive ...")
         sys.exit(2)
 
     if not any([args.simulation, args.fit, args.loss_analysis]):
-        parser.error("either --simulation or --fit or --loss_analysis is required ...")
+        parser.error("either --simulation or --fit or --loss_analysis"
+                     " is required ...")
         sys.exit(2)
 
     if args.input_directory:
-        args.config_file = os.path.join(args.input_directory, "input", "config.json")
-        args.model_file = os.path.join(args.input_directory, "input", "model.json")
-        args.output = os.path.join(args.input_directory, "output")
+        args.config_file = os.path.join(
+            args.input_directory, "input", "config.json")
+        args.model_file = os.path.join(
+            args.input_directory, "input", "model.json")
+        args.output = os.path.join(
+            args.input_directory, "output")
 
 
     if not os.path.isfile(args.config_file):
-        parser.error("Unable to locate config file "+str(args.config_file)+" ...")
+        parser.error(
+            "Unable to locate config file "+str(args.config_file)+" ...")
         sys.exit(2)
 
     if not os.path.isfile(args.model_file):
-        parser.error("Unable to locate model file "+str(args.model_file)+" ...")
+        parser.error(
+            "Unable to locate model file "+str(args.model_file)+" ...")
         sys.exit(2)
 
-    args.output = os.path.join(os.path.dirname(os.path.dirname(args.config_file)), "output")
+    args.output = os.path.join(
+        os.path.dirname(os.path.dirname(args.config_file)), "output")
     print(args.output )
     try:
         if not os.path.exists(args.output):
             os.makedirs(args.output)
     except Exception:
-        parser.error("Unable to create output folder " + str(args.output) + " ...")
+        parser.error(
+            "Unable to create output folder " + str(args.output) + " ...")
         sys.exit(2)
 
     rootLogger.set_log_level(args.verbose)
-
     rootLogger.set_log_file_path(os.path.join(args.output, "log.txt"))
-    rootLogger.info('Simulation initiated...')
+    rootLogger.info('Simulation initiated...\n')
 
     # ---------------------------------------------------------------------
     # Configure simulation model.
@@ -111,20 +124,20 @@ def main():
     hazards = HazardsContainer(config)
     infrastructure = ingest_model(config)
 
+    # ---------------------------------------------------------------------
+    # SIMULATION
+    # Get the results of running a simulation
+    # ---------------------------------------------------------------------
+    # response_list = [
+    #     {},  # hazard level vs component damage state index
+    #     {},  # hazard level vs infrastructure output
+    #     {},  # hazard level vs component response
+    #     {},  # hazard level vs component type response
+    #     [],  # array of infrastructure output per sample
+    #     [],  # array infrastructure econ loss per sample
+    #     {},  # hazard level vs component class dmg level pct
+    #     {}]  # hazard level vs component class expected damage index
     if args.simulation:
-        # ---------------------------------------------------------------------
-        # SIMULATION
-        # Get the results of running a simulation
-        # ---------------------------------------------------------------------
-        response_list = [
-            {},  # hazard level vs component damage state index
-            {},  # hazard level vs infrastructure output
-            {},  # hazard level vs component response
-            {},  # hazard level vs component type response
-            [],  # array of infrastructure output per sample
-            [],  # array infrastructure econ loss per sample
-            {},  # hazard level vs component class dmg level pct
-            {}]  # hazard level vs component class expected damage index
 
         response_list = calculate_response(hazards, scenario, infrastructure)
 
@@ -138,7 +151,8 @@ def main():
         plot_mean_econ_loss(scenario, economic_loss_array, hazards)
 
         if config.HAZARD_INPUT_METHOD == "hazard_array":
-            pe_by_component_class(response_list, infrastructure, scenario, hazards)
+            pe_by_component_class(
+                response_list, infrastructure, scenario, hazards)
 
         # ---------------------------------------------------------------------
         # Visualizations
@@ -148,50 +162,70 @@ def main():
         sys_topology_view.draw_sys_topology(viewcontext="as-built")
         rootLogger.info('Simulation completed...')
 
+    # -------------------------------------------------------------------------
+    # FIT MODEL ANALYSIS
+    # -------------------------------------------------------------------------
     if args.fit:
 
         args.pe_sys = None
-        existing_models = ["potablewatertreatmentplant", "pwtp", "wastewatertreatmentplant", "wwtp","watertreatmentplant", "wtp"]
+        existing_models = [
+            "potablewatertreatmentplant", "pwtp",
+            "wastewatertreatmentplant", "wwtp",
+            "watertreatmentplant", "wtp",
+            "powerstation",
+            "substation"
+            ]
+
         if infrastructure.system_class.lower() == 'powerstation':
-            args.pe_sys = os.path.join(config.RAW_OUTPUT_DIR, 'pe_sys_econloss.npy')
+            args.pe_sys = os.path.join(
+                config.RAW_OUTPUT_DIR, 'pe_sys_econloss.npy')
+
         elif infrastructure.system_class.lower() == 'substation':
-            args.pe_sys = os.path.join(config.RAW_OUTPUT_DIR, 'pe_sys_cpfailrate.npy')
+            args.pe_sys = os.path.join(
+                config.RAW_OUTPUT_DIR, 'pe_sys_cpfailrate.npy')
+
         elif infrastructure.system_class.lower() in existing_models:
-            args.pe_sys = os.path.join(config.RAW_OUTPUT_DIR, 'pe_sys_econloss.npy')
+            args.pe_sys = os.path.join(
+                config.RAW_OUTPUT_DIR, 'pe_sys_econloss.npy')
 
         if args.pe_sys is not None:
-            # ---------------------------------------------------------------------
-            # FIT MODEL ANALYSIS
-            # ---------------------------------------------------------------------
-            rootLogger.info('Start: FIT MODEL ANALYSIS')
+            rootLogger.info('Start: Attempting to fit MODEL to simulation '
+                            'data...')
 
             hazard_scenarios = hazards.hazard_scenario_list
             sys_limit_states = infrastructure.get_system_damage_states()
             pe_sys = np.load(args.pe_sys)
             # Calculate & Plot Fitted Models
-            fit_prob_exceed_model(hazard_scenarios, pe_sys, sys_limit_states, config.OUTPUT_PATH, config)
+            fit_prob_exceed_model(
+                hazard_scenarios,
+                pe_sys,
+                sys_limit_states,
+                config.OUTPUT_PATH,
+                config)
 
-            rootLogger.info('End: FIT MODEL ANALYSIS')
+            rootLogger.info('End: Model fitting complete.')
         else:
-            rootLogger.error("Input  pe_sys file not found: " + str(args.output))
+            rootLogger.error("Input  pe_sys file not found: " +
+                             str(args.output))
 
-    # ---------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     # SCENARIO LOSS ANALYSIS
-    # ---------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     if args.loss_analysis:
 
         args.ct = os.path.join(config.OUTPUT_PATH, 'comptype_response.csv')
         args.cp = os.path.join(config.OUTPUT_PATH, 'component_response.csv')
 
         if args.ct is not None and args.cp is not None:
-            run_scenario_loss_analysis(scenario, hazards, infrastructure, config, args.ct, args.cp)
+            run_scenario_loss_analysis(
+                scenario, hazards, infrastructure, config, args.ct, args.cp)
         else:
             if args.ct is None:
                 rootLogger.error("Input files not found: " + str(args.ct))
             if args.cp is None:
                 rootLogger.error("Input files not found: " + str(args.cp))
 
-    rootLogger.info('End')
+    rootLogger.info('Run Complete.\n')
 
 
 if __name__ == "__main__":
