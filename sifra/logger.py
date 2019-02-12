0d51c8493f3a3fa99fc4b2988c0565df1b7d9437
marufr <maruf.r@outlook.com>
2019-02-12 17:41:28 +1100

    Changed logging method - use dictConfig, and NOT custom logging class.

--- sifra/logger.py
+++ sifra/logger.py
@@ -1,75 +1,38 @@
-import coloredlogs, logging
-import os
-
-coloredlogs.DEFAULT_LOG_FORMAT = '%(asctime)s [%(threadName)s] %(levelname)-8.8s %(message)s'
-
-coloredlogs.COLOREDLOGS_LEVEL_STYLES = 'spam=22;debug=28;verbose=34;notice=220;warning=202;success=118,bold; error=124;critical=background=red'
-
-class Logger():
-    def __init__(self):
-
-        # define logger object to reference in modules and display logs
-        logging.captureWarnings(True)
-        self.logger = logging.getLogger('py.warnings')
-        self.logger.setLevel(logging.DEBUG)
-
-        # handler to display LOGS to CONSOLE
-        # ---------------------------------------------------------------------
-        coloredlogs.install(level='DEBUG')
-        coloredlogs.install(milliseconds=True)
-
-        # handler to save LOGS to FILE
-        # ---------------------------------------------------------------------
-        self.logFormatter = logging.Formatter("%(asctime)s [%(threadName)s] %(levelname)-8.8s  %(message)s")
-
-    def set_log_file_path(self, file_path):
-
-        # if os.path.exists(file_path):
-        directory = os.path.dirname(file_path)
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-
-        self.fileHandler = logging.FileHandler(file_path, mode='w+')
-        self.fileHandler.setFormatter(self.logFormatter)
-        self.logger.addHandler(self.fileHandler)
-
-    # change log level, it has to be a logging.level object eg logging.INFO
-    def set_log_level(self, input_verbose_level):
-
-        level = logging.DEBUG
-
-        if input_verbose_level is not None:
-            if input_verbose_level.upper() == "DEBUG":
-                level = logging.DEBUG
-
-            elif input_verbose_level.upper() == "INFO":
-                level = logging.INFO
-
-            elif input_verbose_level.upper() == "WARNING":
-                level = logging.WARNING
-
-            elif input_verbose_level.upper() == "ERROR":
-                level = logging.ERROR
-
-            elif input_verbose_level.upper() == "CRITICAL":
-                level = logging.CRITICAL
-
-        self.logger.setLevel(level)
-
-    def debug(self, msg):
-        self.logger.debug(msg)
-
-    def info(self, msg):
-        self.logger.info(msg)
-
-    def warning(self, msg):
-        self.logger.warning(msg)
-
-    def error(self, msg):
-        self.logger.error(msg)
-
-    def critical(self, msg):
-        self.logger.critical(msg)
+import logging
+import logging.config
 
 
-rootLogger = Logger()
+def configure_logger(log_path, loglevel='DEBUG'):
+    logging.config.dictConfig({
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'console_formatter': {
+                'format': '%(levelname)-8s '+
+                          '[%(filename)s:%(lineno)s] %(message)s'},
+            'file_formatter': {
+                'format': '%(asctime)s,%(msecs)s - %(levelname)-8s '+
+                          '[%(filename)s:%(lineno)s] %(message)s'}
+        },
+        'handlers': {
+            'console': {
+                'level': loglevel,
+                'class': 'logging.StreamHandler',
+                'formatter': 'console_formatter',
+                'stream': 'ext://sys.stdout'
+            },
+            'file': {
+                'level': 'INFO',
+                'class': 'logging.FileHandler',
+                'formatter': 'file_formatter',
+                'filename': log_path,
+                'mode': 'w'
+            }
+        },
+        'loggers': {
+            '': {
+                'level': 'DEBUG',
+                'handlers': ['console', 'file']
+            }
+        },
+    })
