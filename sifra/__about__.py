3efa9c329c25e9ca192f6ae8be20b23aa1e4490e
marufr <maruf.r@outlook.com>
2019-02-27 11:04:10 +1100

    VERSION update. There are major changes to how the code is run.

--- sifra/__about__.py
+++ sifra/__about__.py
@@ -2,10 +2,10 @@ __packagename__ = "sifra"
 __description__ = "System for Infrastructure Facility Resilience Analysis"
 __url__ = "https://github.com/GeoscienceAustralia/sifra"
 
-__version__ = "0.2.3"
+__version__ = "0.3.0.PRE"
 
 __author__ = "Geoscience Australia"
 __email__ = "maruf.rahman@ga.gov.au"
 
 __license__ = "Apache License, Version 2.0"
-__copyright__ = "2018 %s" % __author__
+__copyright__ = "2019 %s" % __author__
