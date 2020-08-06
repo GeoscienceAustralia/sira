483b9d8a4007590eb5fc10bd63a6312d5ba003a6
Maruf Rahman <maruf.r@outlook.com>
2020-08-06 10:42:56 +1000

    added routines for locating config and model files of required naming structure

--- sira/utilities.py
+++ sira/utilities.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 
 def get_file_name(path):
@@ -11,3 +12,23 @@ def get_dir_path(path):
 
 def get_file_extension(path):
     return os.path.splitext(os.path.basename(path))[1][1:]
+
+
+def get_config_file_path(input_dir):
+    config_file_name = None
+    for fname in os.listdir(input_dir):
+        confmatch = re.search(r"(?i)^config.*\.json$", fname)
+        if confmatch is not None:
+            config_file_name = confmatch.string
+    config_file = os.path.join(input_dir, config_file_name)
+    return config_file
+
+
+def get_model_file_path(input_dir):
+    model_file_name = None
+    for fname in os.listdir(input_dir):
+        modelmatch = re.search(r"(?i)^model.*\.json$", fname)
+        if modelmatch is not None:
+            model_file_name = modelmatch.string
+    model_file = os.path.join(input_dir, model_file_name)
+    return model_file
