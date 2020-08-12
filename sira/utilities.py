644f939c8c4b2285bdf25c11f93c0d753467af89
Maruf Rahman <maruf.r@outlook.com>
2020-08-12 11:39:51 +1000

    removed unneeded functions

--- sira/utilities.py
+++ sira/utilities.py
@@ -6,10 +6,6 @@ def get_file_name(path):
     return os.path.splitext(os.path.basename(path))[0]
 
 
-def get_dir_path(path):
-    return os.path.dirname(path)
-
-
 def get_file_extension(path):
     return os.path.splitext(os.path.basename(path))[1][1:]
 
