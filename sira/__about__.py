60c10982be24a43d46118b9527e30358388dca09
Maruf Rahman <maruf.r@outlook.com>
2020-09-21 10:41:57 +1000

    version and date updated

--- sira/__about__.py
+++ sira/__about__.py
@@ -2,10 +2,10 @@ __packagename__ = "sira"
 __description__ = "Systemic Infrastructure Resilience Analysis"
 __url__ = "https://github.com/GeoscienceAustralia/sira"
 
-__version__ = "0.1.0.PRE"
+__version__ = "0.1.0"
 
 __author__ = "Geoscience Australia"
 __email__ = "maruf.rahman@ga.gov.au"
 
 __license__ = "Apache License, Version 2.0"
-__copyright__ = "2019 %s" % __author__
+__copyright__ = "2020 %s" % __author__
