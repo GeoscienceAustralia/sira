e2c9f1631f8df4896425f5e32db4bebf446197de
Maruf Rahman <maruf.r@outlook.com>
2020-09-21 23:00:17 +1000

    resinstated full cli test simulation

--- tests/test_simulated_user_run.py
+++ tests/test_simulated_user_run.py
@@ -5,7 +5,7 @@ from pathlib import Path
 # Add the source dir to system path
 import sys
 src_dir = Path(__file__).resolve().parent.parent
-sys.path.append(str(src_dir))
+sys.path.insert(0, str(src_dir))
 
 
 class TestModuleRunProcess(unittest.TestCase):
@@ -37,24 +37,24 @@ class TestModuleRunProcess(unittest.TestCase):
         # An exit status of 0 typically indicates process ran successfully:
         self.assertEqual(exitstatus, 0)
 
-    # def test_full_term_run_wtpmodel(self):
-    #     """
-    #     This module tests:
-    #     - running the application from the terminal, for
-    #     - damage simulation to a water treatment plant model, followed by
-    #     - fragility curve fitting, and
-    #     - loss & recovery analysis.
-    #     """
-    #     model_name = 'potable_water_treatment_plant_A'
-    #     inputdir = Path(self.mdls_dir, model_name)
-    #     process = subprocess.run(
-    #         ['python', str(self.code_dir), '-d', str(inputdir), '-sfl'], 
-    #         stdout=subprocess.PIPE, 
-    #         universal_newlines=True)
-    #     exitstatus = process.returncode
-    #     print(process.stdout)
-    #     # An exit status of 0 typically indicates process ran successfully:
-    #     self.assertEqual(exitstatus, 0)
+    def test_full_term_run_wtpmodel(self):
+        """
+        This module tests:
+        - running the application from the terminal, for
+        - damage simulation to a water treatment plant model, followed by
+        - fragility curve fitting, and
+        - loss & recovery analysis.
+        """
+        model_name = 'potable_water_treatment_plant_A'
+        inputdir = Path(self.mdls_dir, model_name)
+        process = subprocess.run(
+            ['python', str(self.code_dir), '-d', str(inputdir), '-sfl'], 
+            stdout=subprocess.PIPE, 
+            universal_newlines=True)
+        exitstatus = process.returncode
+        print(process.stdout)
+        # An exit status of 0 typically indicates process ran successfully:
+        self.assertEqual(exitstatus, 0)
 
 
 if __name__ == '__main__':
