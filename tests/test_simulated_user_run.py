748dcae1412de93aba39ad54f18307a95be0833c
Maruf Rahman <maruf.r@outlook.com>
2020-12-22 13:40:14 +1100

    exapanded tests

--- tests/test_simulated_user_run.py
+++ tests/test_simulated_user_run.py
@@ -5,13 +5,14 @@ from pathlib import Path
 # ------------------------------------------------------------------------------
 
 testdata = [
-    ('', 'powerstation_coal_A', '-s'),
-    ('', 'substation_tx_230kv', '-sl'),
-    ('', 'potable_water_treatment_plant_A', '-sfl'),
-    ('', 'test_network__basic', '-s')
+    ('_', 'powerstation_coal_A', '-s'),
+    ('_', 'substation_tx_230kv', '-sl'),
+    ('_', 'potable_water_treatment_plant_A', '-sfl'),
+    ('_', 'test_network__basic', '-s'),
+    ('_', 'test_structure__parallel_piecewise', '-s')
 ]
-
 # ------------------------------------------------------------------------------
+
 
 @pytest.mark.modelrun
 @pytest.mark.parametrize(
@@ -21,25 +22,19 @@ testdata = [
 def test_run_model(dir_setup, model_name, run_arg):
     """
     This module tests:
-    running the application from the terminal,
-    for a powerstation model.
+    directly running models from the terminal.
     """
-
     code_dir, mdls_dir = dir_setup
-
     inputdir = Path(mdls_dir, model_name)
     cmd = ['python', str(code_dir), '-d', str(inputdir), run_arg]
 
     process = subprocess.run(
-        cmd,
-        stdout=subprocess.PIPE,
-        universal_newlines=True)
+        cmd, stdout=subprocess.PIPE, universal_newlines=True)
     exitstatus = process.returncode
 
     # An exit status of 0 typically indicates process ran successfully:
     assert exitstatus == 0, f"Run failed for {model_name} with args {run_arg}"
 
-# ------------------------------------------------------------------------------
 
 if __name__ == '__main__':
     pytest.main([__file__])
